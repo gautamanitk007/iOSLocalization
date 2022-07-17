4e2297e0a4c4a9d7bf6e6b7bb711a3a9e4a20667
Gautam kumar singh <gautam.singh@konicaminolta.com>
2022-07-17 23:32:34 +0800

    added public and static

--- Sources/iOSLocalization/iOSLocalization.swift
+++ Sources/iOSLocalization/iOSLocalization.swift
@@ -1,4 +1,5 @@
-struct iOSLocalization {
-    var text = "Hello, World!"
-    var text2 = "gautam"
+public struct iOSLocalization {
+    public var text = "Hello, World!"
+    public static var text2 = "gautam"
+    public static var text3 = "Singh"
 }
